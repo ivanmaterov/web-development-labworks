8bb89a771c53a0337c5f26753b450b77fb21bcfc
ivan <ivan.materov@saritasa.com>
2022-05-24 21:00:49 +0700

    WIP

--- backend/apps/product/api/serializers.py
+++ backend/apps/product/api/serializers.py
@@ -1,16 +1,25 @@
+from unicodedata import category
 from rest_framework import serializers
 
 from apps.product.models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     """Serializer for categories."""
     class Meta:
         model = Category
-        fields = ['id', 'title']
+        fields = [
+            'id',
+            'title',
+        ]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    category = CategorySerializer()
+
     """Serializer for products."""
     class Meta:
         model = Product
